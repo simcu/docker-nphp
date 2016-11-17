bdcd49654280e42c3466ba2dfa327f7f71d54e26
xRain <xrain@simcu.com>
2016-11-18 01:16:41 +0800

    fix wrong config file

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine
 COPY files /home/install
 RUN echo 'http://dl-cdn.alpinelinux.org/alpine/edge/testing' >> /etc/apk/repositories && \
 		apk add --update php5-curl php php5-pdo php5-pdo_mysql php5-mcrypt php5-openssl php5-fpm php5-zip php5-xml php5-mysql \
-		php5-xmlrpc php5-pear php5-sockets php5-gettext php5-iconv php5-pdo_pgsql php5-pdo_sqlite php5-zlib php5-mysqli php5-redis\
+		php5-xmlrpc php5-pear php5-sockets php5-gettext php5-iconv php5-pdo_pgsql php5-pdo_sqlite php5-zlib php5-mysqli php5-redis \
 		php5-bz2 php5-json php5-dom php5-phar php5-ctype php5-gd php5-bcmath php5-memcached php5-soap nginx sqlite && \
         sh /home/install/config.sh && rm -rf /home/install && rm -rf /var/cache/apk/*
 CMD "/run.sh"
--- files/config.sh
+++ files/config.sh
@@ -3,7 +3,7 @@ cp -r /home/install/nginx.conf /etc/nginx/nginx.conf
 cp -r /home/install/index.php /home/index.php
 cp -r /home/install/run.sh /run.sh
 chmod +x /run.sh
-sed -i "s/max_execution_time = 30/max_execution_time = 300/g" /etc/php/php.ini
-sed -i "s/post_max_size = 8M/post_max_size = 1000M/g" /etc/php/php.ini
-sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 1000M/g" /etc/php/php.ini
-sed -i "s/expose_php = On/expose_php = Off/g" /etc/php/php.ini
+sed -i "s/max_execution_time = 30/max_execution_time = 300/g" /etc/php5/php.ini
+sed -i "s/post_max_size = 8M/post_max_size = 1000M/g" /etc/php5/php.ini
+sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 1000M/g" /etc/php5/php.ini
+sed -i "s/expose_php = On/expose_php = Off/g" /etc/php5/php.ini
